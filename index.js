3e4997fd7ff2f0074077754cb414eb6219363a09
Biwceni <fabio.ormeni@hotmail.com>
2022-12-19 19:15:09 -0300

    Server Final

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.use(session({
     saveUninitialized: false,
     proxy: true,
     cookie: {
-        httpOnly: true,
+        httpOnly: false,
         secure: true,
         sameSite: "none",
         maxAge: 1000000
