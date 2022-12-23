2bfa6f4d27a69fd7648271c2f47d8aad146e54be
Biwceni <fabio.ormeni@hotmail.com>
2022-12-23 20:15:28 -0300

    Server Final

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.use(cors({
 app.use(cookieParser());
 
 // Configurando o recebimento de dados para que mantenham um mesmo formato
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Passando os parâmetros para a criação da sessão
 
