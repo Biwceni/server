ef403bce69964a9fabc826137227ca7ffe71a2b0
Biwceni <fabio.ormeni@hotmail.com>
2022-12-30 21:41:38 -0300

    Server Final

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Sintonizando a transição entre os dados do Front-End e do Back-End, para que a conexão entre as partes e a transição entre os dados seja estabelecida
-app.set("trust proxy", 1);
+// app.set("trust proxy", 1);
+app.set("trust proxy", ["35.160.120.126", "44.233.151.27", "34.211.200.85"]);
 
 // Passando os parâmetros para a criação da sessão
 app.use(session({
